015955aa3d8e3722ebb5e63a31932742819a47b9
Jay L <jay@Jays-iMac.local>
2021-06-14 11:17:23 +0800

    add tooltip for color swatches too

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 (function($) {
     $(document).ready(function(){
-        $('.filter-item-list .filter-image').attr('data-toggle', 'tooltip').attr('data-placement', 'top');
-        $('.filter-item-list .filter-image').each(function(){
+        $('.filter-item-list .filter-image, .filter-item-list .filter-color').attr('data-toggle', 'tooltip').attr('data-placement', 'top');
+        $('.filter-item-list .filter-image, .filter-item-list .filter-color').each(function(){
             $(this).attr('title', $(this).text());
             $(this).tooltip();
         });
